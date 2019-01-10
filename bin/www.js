9a75ee3f05b2c831575ec3b477ddc4c136bce17b
luodantong <474376875@qq.com>
2019-01-10 17:18:29 +0800

    删除冗余代码，将区块信息存入文件，新增爬取房源单价总价信息

--- bin/www.js
+++ bin/www.js
@@ -11,8 +11,9 @@ var cheerio = require("cheerio");
 var fs = require("fs");
 
 var baseUrl = "https://gz.lianjia.com/ershoufang/"; //初始网页
-var urlArr = []; //url数组
+var urlArr = []; //区块url数组
 
+// 将Unicode转化为中文
 function decodeUnicode(str) {
   return str
     ? unescape(
@@ -25,12 +26,12 @@ function decodeUnicode(str) {
 }
 
 app.get("/", function(req, res, next) {
-  // 命令 ep 重复监听 emit事件(get_topic_html)，当get_topic_html爬取完毕之后执行
-  ep.after("get_topic_html", 1, function(eps) {
+  // 命令 ep 重复监听 emit事件(get_district_html)，当get_district_html爬取完毕之后执行
+  ep.after("get_district_html", 1, function(eps) {
     var concurrencyCount = 0;
     var num = -4; //因为是5个并发，所以需要减4
 
-    // 利用callback函数将结果返回去，然后在结果中取出整个结果数组。
+    // 对于每个url第一页，利用callback函数将结果返回去，然后在结果中取出整个结果数组。
     var fetchUrl = function(myurl, callback) {
       var fetchStart = new Date().getTime();
       concurrencyCount++;
@@ -52,14 +53,16 @@ app.get("/", function(req, res, next) {
 
           var $ = cheerio.load(ssres.text);
 
-          // 对获取的结果进行处理函数
+          var totalPage = $(".fr div a").html();
+
+          // 对每页获取的结果进行处理函数
           getDownloadLink($, function(obj) {
             res.write("<br/>");
             res.write("url-->  " + myurl);
             res.write("<br/>");
             res.write("House number-->  " + obj.houseNum);
             res.write("<br/>");
-            res.write("house name-->  " + obj.houses[0].name + " 等");
+            res.write("price-->  " + obj.houses[0].totalPrice);
             res.write("<br/>");
 
             //存为json文件
@@ -87,13 +90,13 @@ app.get("/", function(req, res, next) {
       function(err, result) {
         // 爬虫结束后的回调，可以做一些统计结果
         console.log("抓包结束，一共抓取了-->" + urlArr.length + "条数据");
-        urlArr = [];
+        urlArr = []; //清空url数组
         return false;
       }
     );
   });
   (function(page) {
-    //先抓取链家广州二手房首页
+    //先抓取链家广州二手房首页地区信息
     superagent
       .get(page)
       .charset("utf-8")
@@ -115,19 +118,24 @@ app.get("/", function(req, res, next) {
             var tem = {};
             tem.name = $(this).text(); //区名
             tem.href = "https://gz.lianjia.com" + $(this).attr("href"); //url
+            tem.href = []; //页数数组
             urlArr.push(tem.href);
             position.push(tem);
           });
         /*
          *流程控制语句
-         *当区域链接爬取完毕之后，我们就开始爬取各区二手房
+         *当区域链接爬取完毕之后，开始爬取各区二手房
          */
-        ep.emit("get_topic_html", "get " + page + " successful");
+        const positionJson = { position: position };
+        var positionFilename =
+          "D:\\pro_gra_sample\\express_demo\\position.json";
+        fs.writeFileSync(positionFilename, JSON.stringify(positionJson));
+        ep.emit("get_district_html", "get " + page + " successful");
       });
   })(baseUrl);
 });
 
-// 获取下载链接
+// 获取房源信息
 function getDownloadLink($, callback) {
   var houseNum = $(".total span").text();
 
@@ -174,9 +182,20 @@ function getDownloadLink($, callback) {
         .find("a")
         .html()
     );
+    //房子总价
+    house.totalPrice = decodeUnicode(
+      $(this)
+        .find(".totalPrice span")
+        .text()
+    );
+    // 房子单价
+    house.unitPrice = decodeUnicode(
+      $(this)
+        .find(".unitPrice")
+        .attr("data-price")
+    );
 
     houses.push(house);
-    console.log(houseInfo);
   });
 
   var obj = {
@@ -188,93 +207,6 @@ function getDownloadLink($, callback) {
   }
   callback(obj);
 }
-
-// app.get("/", function(req, res) {
-//   request("https://gz.lianjia.com/ershoufang/", function(
-//     error,
-//     response,
-//     body
-//   ) {
-//     if (!error && response.statusCode == 200) {
-//       $ = cheerio.load(body);
-//       // 区域数组
-//       var position = [];
-//       $(".position dl")
-//         .eq(1)
-//         .find("dd div div")
-//         .eq(0)
-//         .find("a")
-//         .each(function() {
-//           var tem = {};
-//           tem.name = $(this).text(); //区名
-//           tem.href = "https://gz.lianjia.com" + $(this).attr("href"); //url
-//           position.push(tem);
-//         });
-
-//       //二手房对象数组
-//       var houses = [];
-//       var houses_list = $(".LOGCLICKDATA");
-//       $(".LOGCLICKDATA").each(function() {
-//         var house = {};
-//         //标题
-//         house.titleName = decodeUnicode(
-//           $(this)
-//             .find(".info .title")
-//             .find("a")
-//             .html()
-//         );
-//         //楼盘名
-//         house.name = decodeUnicode(
-//           $(this)
-//             .find(".houseInfo")
-//             .find("a")
-//             .html()
-//         );
-
-//         var houseInfo = [];
-//         houseInfo = decodeUnicode(
-//           $(this)
-//             .find(".houseInfo")
-//             .text()
-//         ).split(" | ");
-//         //房屋户型
-//         house.layout = houseInfo[1];
-//         //大小
-//         house.size = houseInfo[2];
-//         //朝向
-//         house.toward = houseInfo[3];
-//         //装修
-//         house.decoration = houseInfo[4];
-//         //电梯
-//         house.elevator = houseInfo[5] ? houseInfo[5] : "";
-
-//         //地址
-//         house.positionInfo = decodeUnicode(
-//           $(this)
-//             .find(".positionInfo")
-//             .find("a")
-//             .html()
-//         );
-//         //控制台输出楼盘名
-//         // console.log(house.name);
-//         houses.push(house);
-//       });
-
-//       res.json({
-//         position: position
-//       });
-
-//       const positionJson = { position: position };
-//       var positionFilename = "D:\\pro_gra_sample\\express_demo\\position.json";
-//       fs.writeFileSync(positionFilename, JSON.stringify(positionJson));
-
-//       // const houseJson = { houses: houses };
-//       // var houseFilename = "D:\\pro_gra_sample\\express_demo\\houses.json";
-//       // fs.writeFileSync(houseFilename, JSON.stringify(houseJson));
-//     }
-//   });
-// });
-
 var server = app.listen(8080, function() {
   console.log("listening at 8080");
 });
