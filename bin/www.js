450d17a89baae7db4bcd58ac3781cf8b7a1aaba7
luodantong <474376875@qq.com>
2019-01-26 03:08:50 +0800

    用eventproxy初步解耦

--- bin/www.js
+++ bin/www.js
@@ -17,6 +17,11 @@ var errLength = []; //统计出错的链接数
 var urlArr = []; //区块url数组
 var urlPage = []; //各区块全部页数url数组
 
+var concurrencyCount = 0;
+var num = -4; //因为是5个并发，所以需要减4
+var concurrencyCount1 = 0;
+var num1 = -5; //因为是6个并发，所以需要减5
+
 // 将Unicode转化为中文
 function decodeUnicode(str) {
   return str
@@ -30,55 +35,12 @@ function decodeUnicode(str) {
 }
 
 app.get("/", function(req, res, next) {
-  // 命令 ep 重复监听 emit事件(get_district_html)，当get_district_html爬取完毕之后执行
-  ep.after("get_district_html", 1, function(eps) {
-    var concurrencyCount = 0;
-    var num = -4; //因为是5个并发，所以需要减4
-
-    // 爬取各区块全部页数
-    var fetchUrl = function(myurl, callback) {
-      var fetchStart = new Date().getTime();
-      concurrencyCount++;
-      num += 1;
-      console.log("现在的并发数是", concurrencyCount, "，正在抓取的是", myurl);
-      superagent
-        .get(myurl)
-        .charset("utf-8") //解决编码问题
-        .end(function(err, ssres) {
-          if (err) {
-            callback(err, myurl + " error happened!");
-            errLength.push(myurl);
-            return next(err);
-          }
-
-          var time = new Date().getTime() - fetchStart;
-          console.log("抓取 " + myurl + " 成功", "，耗时" + time + "毫秒");
-          concurrencyCount--;
-
-          var $ = cheerio.load(ssres.text);
-
-          // 获取总页数
-          var totalPage = $(".house-lst-page-box").attr("page-data")
-            ? $(".house-lst-page-box")
-                .attr("page-data")
-                .split(",")[0]
-                .split(":")[1]
-            : 1;
-
-          //生成各区块全部页数url数组
-          for (let i = 1; i <= 1; i++) {
-            urlPage.push(myurl + "pg" + i + "/");
-          }
-
-          var result = {
-            movieLink: myurl
-          };
-          callback(null, result);
-        });
-    };
+  // 命令 ep 重复监听 emit事件(getUrlQueue)，当getUrlQueue爬取完毕之后执行
+  ep.after("getUrlQueue", 1, function(list) {
+    // var concurrencyCount = 0;
+    // var num = -4; //因为是5个并发，所以需要减4
 
     // 控制最大并发数为5，在结果中取出callback返回来的整个结果数组。
-    // mapLimit(arr, limit, iterator, [callback])
     async.mapLimit(
       urlArr,
       5,
@@ -88,82 +50,14 @@ app.get("/", function(req, res, next) {
       function(err, result) {
         //爬取全部页面url结束后，对各页面url数组二次爬取房源信息
         // 爬取房源信息
-        var concurrencyCount1 = 0;
-        var num1 = -5; //因为是6个并发，所以需要减5
+        // var concurrencyCount1 = 0;
+        // var num1 = -5; //因为是6个并发，所以需要减5
 
-        var fetchInfo = function(myurl, callback) {
-          var fetchStart1 = new Date().getTime();
-          concurrencyCount1++;
-          num1 += 1;
-          console.log(
-            "现在的并发数是",
-            concurrencyCount1,
-            "，正在抓取的是",
-            myurl
-          );
-
-          superagent
-            .get(myurl)
-            .charset("utf-8") //解决编码问题
-            .end(function(err, ssres) {
-              if (err) {
-                callback(err, myurl + " error happened!");
-                errLength.push(myurl);
-                return next(err);
-              }
-
-              var time1 = new Date().getTime() - fetchStart1;
-              console.log("抓取 " + myurl + " 成功", "，耗时" + time1 + "毫秒");
-              concurrencyCount1--;
-
-              var $ = cheerio.load(ssres.text);
-
-              // 对每页获取的结果进行处理函数
-              getDownloadLink($, function(obj) {
-                res.write("<br/>");
-                res.write("url-->  " + myurl);
-                res.write("<br/>");
-
-                //存为json文件
-                var fileName =
-                  "D:\\pro_gra_sample\\express_demo\\" +
-                  myurl.split("/")[4] +
-                  ".json";
-                fs.writeFileSync(fileName, JSON.stringify(obj));
-
-                // 存入数据库
-                var colName = myurl.split("/")[4];
-                MongoClient.connect(
-                  urldb,
-                  function(err, db) {
-                    if (err) throw err;
-                    var dbo = db.db("lianjiaSpider");
-                    dbo
-                      .collection(colName)
-                      .insertMany(obj.houses, function(err, res) {
-                        if (err) {
-                          console.log("数据库插入", myurl, "出错了");
-                          console.log("obj.houses:", obj.houses);
-                          throw err;
-                        }
-                        console.log("插入的文档数量为: " + res.insertedCount);
-                        db.close();
-                      });
-                  }
-                );
-              });
-
-              var result = {
-                movieLink: myurl
-              };
-              callback(null, result);
-            });
-        };
         async.mapLimit(
           urlPage,
           6,
           function(myurl, callback) {
-            fetchInfo(myurl, callback);
+            DownloadHtml(res, myurl, callback);
           },
           function(err, result) {
             // 爬虫结束后的回调，可以做一些统计结果
@@ -179,66 +73,126 @@ app.get("/", function(req, res, next) {
   });
   (function(page) {
     //先抓取链家广州二手房首页地区信息
-    superagent
-      .get(page)
-      .charset("utf-8")
-      .end(function(err, sres) {
-        // 常规的错误处理
-        if (err) {
-          console.log("抓取" + page + "这条信息的时候出错了");
-          return next(err);
-        }
-        var $ = cheerio.load(sres.text);
-        // 区域数组
-        var position = [];
-        $(".position dl")
-          .eq(1)
-          .find("dd div div")
-          .eq(0)
-          .find("a")
-          .each(function() {
-            var tem = {};
-            tem.name = $(this).text(); //区名
-            tem.href = "https://gz.lianjia.com" + $(this).attr("href"); //url
-            urlArr.push(tem.href);
-            position.push(tem);
-          });
-        /*
-         *流程控制语句
-         *当区域链接爬取完毕之后，开始爬取各区二手房
-         */
-        const positionJson = { position: position };
-        var positionFilename =
-          "D:\\pro_gra_sample\\express_demo\\position.json";
-        fs.writeFileSync(positionFilename, JSON.stringify(positionJson));
-
-        //存入数据库
-        MongoClient.connect(
-          urldb,
-          function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("lianjiaSpider");
-            dbo.collection("position").insertMany(position, function(err, res) {
-              if (err) throw err;
-              console.log("插入的文档数量为: " + res.insertedCount);
-              db.close();
-            });
-          }
-        );
-
-        ep.emit("get_district_html", "get " + page + " successful");
-      });
+    GetUrlQueue(page);
   })(baseUrl);
 });
 
-// 获取房源信息
-function getDownloadLink($, callback) {
-  // var houseNum = $(".total span").text();
-  // var housePage = $(".house-lst-page-box")
-  //   .attr("page-data")
-  //   .split(",")[0]
-  //   .split(":")[1];
+//******************************************************************************* */
 
+//从种子 url 页面获取各区块 url 地址
+function GetUrlQueue(page) {
+  superagent
+    .get(page)
+    .charset("utf-8")
+    .end(function(err, sres) {
+      // 常规的错误处理
+      if (err) {
+        console.log("抓取" + page + "这条信息的时候出错了");
+        return next(err);
+      }
+      var $ = cheerio.load(sres.text);
+      // 区域数组
+      var position = [];
+      $(".position dl")
+        .eq(1)
+        .find("dd div div")
+        .eq(0)
+        .find("a")
+        .each(function() {
+          var tem = {};
+          tem.name = $(this).text(); //区名
+          tem.href = "https://gz.lianjia.com" + $(this).attr("href"); //url
+          urlArr.push(tem.href);
+          position.push(tem);
+        });
+      /*
+       *流程控制语句
+       *当区域链接爬取完毕之后，开始爬取各区二手房
+       */
+      const positionJson = { position: position };
+      var positionFilename = "D:\\pro_gra_sample\\express_demo\\position.json";
+      fs.writeFileSync(positionFilename, JSON.stringify(positionJson));
+
+      //存入数据库
+      MongoClient.connect(
+        urldb,
+        function(err, db) {
+          if (err) throw err;
+          var dbo = db.db("lianjiaSpider");
+          dbo.collection("position").insertMany(position, function(err, res) {
+            if (err) throw err;
+            console.log("插入的文档数量为: " + res.insertedCount);
+            db.close();
+          });
+        }
+      );
+
+      ep.emit("getUrlQueue", "get " + page + " successful");
+    });
+}
+
+// 爬取各区块全部页数
+var fetchUrl = function(myurl, callback) {
+  var fetchStart = new Date().getTime();
+  concurrencyCount++;
+  num += 1;
+  console.log("现在的并发数是", concurrencyCount, "，正在抓取的是", myurl);
+  superagent
+    .get(myurl)
+    .charset("utf-8") //解决编码问题
+    .end(function(err, ssres) {
+      if (err) {
+        callback(err, myurl + " error happened!");
+        errLength.push(myurl);
+        return next(err);
+      }
+
+      var time = new Date().getTime() - fetchStart;
+      console.log("抓取 " + myurl + " 成功", "，耗时" + time + "毫秒");
+      concurrencyCount--;
+
+      var $ = cheerio.load(ssres.text);
+
+      // 获取总页数
+      var totalPage = $(".house-lst-page-box").attr("page-data")
+        ? $(".house-lst-page-box")
+            .attr("page-data")
+            .split(",")[0]
+            .split(":")[1]
+        : 1;
+
+      //生成各区块全部页数url数组
+      for (let i = 1; i <= 1; i++) {
+        urlPage.push(myurl + "pg" + i + "/");
+      }
+
+      var result = {
+        movieLink: myurl
+      };
+      callback(null, result);
+    });
+};
+
+// 循环爬取 positionUrl 队列，从页面中获取各区块数据总页数，生成子url
+function GetPageQueue() {
+  var concurrencyCount = 0;
+  var num = -4; //因为是5个并发，所以需要减4
+  // 控制最大并发数为5，在结果中取出callback返回来的整个结果数组。
+  async.mapLimit(
+    urlArr,
+    5,
+    function(myurl, callback) {
+      fetchUrl(myurl, callback);
+    },
+    function(err, result) {
+      //获取全部url结束后，对各页面url数组二次爬取房源信息
+      ep.emit("GetPageQueue", result);
+    }
+  );
+}
+
+// 获取房源信息
+function AnalysisHtml($, callback) {
   var houses = [];
   $(".listContent li").each(function() {
     var house = {};
@@ -269,19 +223,6 @@ function getDownloadLink($, callback) {
         .find("a")
         .html()
     ).split(" ")[2];
-
-    // var houseInfo = [];
-    // houseInfo = decodeUnicode(
-    //   $(this)
-    //     .find(".houseInfo")
-    //     .text()
-    // ).split(" | ");
-    // //朝向
-    // house.toward = houseInfo[0] ? houseInfo[0] : "";
-    // //装修
-    // house.decoration = houseInfo[1] ? houseInfo[1] : "";
-    // //电梯
-    // house.elevator = houseInfo[2] ? houseInfo[2] : "";
 
     //挂牌总价
     house.listedPrice = decodeUnicode(
@@ -319,15 +260,72 @@ function getDownloadLink($, callback) {
   });
 
   var obj = {
-    // houseNum: houseNum,
-    // housePage: housePage,
     houses: houses
   };
-  // if (!houseNum) {
-  //   houseNum = "暂无房源";
-  // }
   callback(obj);
 }
+
+//异步并发爬取爬取 pageUrl 队列，获取所需数据
+function DownloadHtml(res, myurl, callback) {
+  var fetchStart1 = new Date().getTime();
+  concurrencyCount1++;
+  num1 += 1;
+  console.log("现在的并发数是", concurrencyCount1, "，正在抓取的是", myurl);
+
+  superagent
+    .get(myurl)
+    .charset("utf-8") //解决编码问题
+    .end(function(err, ssres) {
+      if (err) {
+        callback(err, myurl + " error happened!");
+        errLength.push(myurl);
+        return next(err);
+      }
+
+      var time1 = new Date().getTime() - fetchStart1;
+      console.log("抓取 " + myurl + " 成功", "，耗时" + time1 + "毫秒");
+      concurrencyCount1--;
+
+      var $ = cheerio.load(ssres.text);
+
+      // 对每页获取的结果进行处理函数
+      AnalysisHtml($, function(obj) {
+        res.write("<br/>");
+        res.write("url-->  " + myurl);
+        res.write("<br/>");
+
+        //存为json文件
+        var fileName =
+          "D:\\pro_gra_sample\\express_demo\\" + myurl.split("/")[4] + ".json";
+        fs.writeFileSync(fileName, JSON.stringify(obj));
+
+        // 存入数据库
+        var colName = myurl.split("/")[4];
+        MongoClient.connect(
+          urldb,
+          function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("lianjiaSpider");
+            dbo.collection(colName).insertMany(obj.houses, function(err, res) {
+              if (err) {
+                console.log("数据库插入", myurl, "出错了");
+                console.log("obj.houses:", obj.houses);
+                throw err;
+              }
+              console.log("插入的文档数量为: " + res.insertedCount);
+              db.close();
+            });
+          }
+        );
+      });
+
+      var result = {
+        movieLink: myurl
+      };
+      callback(null, result);
+    });
+}
+
 var server = app.listen(8080, function() {
   console.log("listening at 8080");
 });
